4a4899e43f29864c3076b6704c61ba9ade288847
Stefan Etarsky <stefan.etarsky@gmail.com>
2024-10-12 21:15:59 -0400

    Hello world with Vue.js

---
+++ README.md
@@ -0,0 +1,3 @@
+## Flask - Vue - Crud
+
+This is a project to learn how to connect Vue to Flask, following along [these instructions](https://testdriven.io/blog/developing-a-single-page-app-with-flask-and-vuejs/)
--- server/app.py
+++ server/app.py
@@ -6,10 +6,11 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 
 # Enable CORS
+# In a production environment, you should only allow cross-origin requests 
+# from the domain where the front-end application is hosted.
 CORS(app, resources={r'/*': {'origins': '*'}})
 
 # Sanity check route
-
 @app.route('/ping', methods=['GET'])
 def ping_pong():
     return jsonify('pong!')
