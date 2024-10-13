54afad00267079e2ff38aa8df2cd1a00b5ffb023
Stefan Etarsky <stefan.etarsky@gmail.com>
2024-10-13 10:31:58 -0400

    Add bootstrap styled books table

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,10 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Books from '../components/Books.vue'
 import Ping from '../components/Ping.vue'
 
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: '/',
+      name: 'Books',
+      component: Books
+    },
     {
       path: '/ping',
       name: 'ping',
