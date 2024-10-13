daecac8994208614582aaa72fc8f2b5255bb5527
Stefan Etarsky <stefan.etarsky@gmail.com>
2024-10-13 11:15:10 -0400

    Implement GET route

--- server/app.py
+++ server/app.py
@@ -10,11 +10,36 @@ app.config.from_object(__name__)
 # from the domain where the front-end application is hosted.
 CORS(app, resources={r'/*': {'origins': '*'}})
 
+BOOKS = [
+    {
+        'title': 'On the Road',
+        'author': 'Jack Kerouac',
+        'read': True
+    },
+    {
+        'title': 'Harry Potter and the Philosopher\'s Stone',
+        'author': 'J. K. Rowling',
+        'read': False
+    },
+    {
+        'title': 'Green Eggs and Ham',
+        'author': 'Dr. Seuss',
+        'read': True
+    }
+]
+
 # Sanity check route
 @app.route('/ping', methods=['GET'])
 def ping_pong():
     return jsonify('pong!')
 
+# Route to get all books
+@app.route('/books', methods=['GET'])
+def all_books():
+    return jsonify({
+        'status': 'success',
+        'books': BOOKS
+    })
 
 if __name__ == '__main__':
     app.run()
